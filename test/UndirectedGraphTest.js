935eada8dc368eebeb92775e35535521568fade5
HeechanYang <yhc944@gmail.com>
2018-12-22 03:18:17 +0900

    [ADD] UndirectedGraph. closed #4

--- test/UndirectedGraphTest.js
+++ test/UndirectedGraphTest.js
@@ -1,7 +1,9 @@
-let Graph = require('../lib/Graph.js');
-let Vertex = Graph.Vertex;
+let UndirectedGraph = require('../lib/UndirectedGraph.js');
+let Vertex = UndirectedGraph.Vertex;
 
-let graph = new Graph();
+let graph = new UndirectedGraph();
+
+console.log(UndirectedGraph.Vertex);
 
 let v1 = new Vertex();
 let v2 = new Vertex();
@@ -21,16 +23,16 @@ graph.addVertex(v6);
 graph.addEdge(v1, v2, 1);
 graph.addEdge(v1, v3, 2);
 graph.addEdge(v2, v3, 3);
-graph.addEdge(v3, v1, 2);
 graph.addEdge(v3, v4, 2);
 graph.addEdge(v5, v6, 1);
 graph.addEdge(v6, v1, 2);
 
+
 if(graph.vertexSize !== 6){
-    throw Error("vertexSize");
+    throw Error("vertexSize : " + graph.vertexSize);
 }
-if(graph.edgeSize !== 7){
-    throw Error("edgeSize");
+if(graph.edgeSize !== 6){
+    throw Error("edgeSize : " + graph.edgeSize);
 }
 
 console.log(graph.getVertexIterator());
