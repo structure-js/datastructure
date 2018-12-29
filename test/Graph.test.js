96d09042f9d2f756f9a7ff0feb20d46ae5b110eb
HeechanYang <yhc944@gmail.com>
2018-12-30 03:34:47 +0900

    [UPDATE] Graph functions don't need Vertex type params. close #64

--- test/Graph.test.js
+++ test/Graph.test.js
@@ -1,15 +1,6 @@
 let Graph = require('../index').Graph;
-let Vertex = Graph.Vertex;
 
 let graph = new Graph();
-
-let v1 = new Vertex('A');
-let v2 = new Vertex('B');
-let v3 = new Vertex('C');
-let v4 = new Vertex('D');
-let v5 = new Vertex('E');
-let v6 = new Vertex('F');
-let v7 = new Vertex('G');
 
 test('When construct new Graph,', () => {
     expect(graph.vertexSize).toBe(0);
@@ -17,49 +8,49 @@ test('When construct new Graph,', () => {
 });
 
 test('graph.addVertex(vertex)', () => {
-    graph.addVertex(v1);
-    graph.addVertex(v2);
-    graph.addVertex(v3);
-    graph.addVertex(v4);
-    graph.addVertex(v5);
-    graph.addVertex(v6);
-    graph.addVertex(v7);
+    graph.addVertex('A');
+    graph.addVertex('B');
+    graph.addVertex('C');
+    graph.addVertex('D');
+    graph.addVertex('E');
+    graph.addVertex('F');
+    graph.addVertex('G');
 
     expect(graph.vertexSize).toBe(7);
     expect(graph.edgeSize).toBe(0);
 });
 
 test('graph.addEdge(vs, ve, weight)', () => {
-    graph.addEdge(v1, v2, 1);
-    graph.addEdge(v1, v3, 2);
-    graph.addEdge(v2, v3, 3);
-    graph.addEdge(v3, v1, 2);
-    graph.addEdge(v3, v4, 3);
-    graph.addEdge(v3, v5, 2);
-    graph.addEdge(v4, v6, 4);
-    graph.addEdge(v5, v6, 1);
-    graph.addEdge(v5, v7, 2);
-    graph.addEdge(v7, v1, 1);
-    graph.addEdge(v7, v2, 2);
-    // graph.addEdge(v6, v1, 2);
+    graph.addEdge('A', 'B', 1);
+    graph.addEdge('A', 'C', 2);
+    graph.addEdge('B', 'C', 3);
+    graph.addEdge('C', 'A', 2);
+    graph.addEdge('C', 'D', 3);
+    graph.addEdge('C', 'E', 2);
+    graph.addEdge('D', 'F', 4);
+    graph.addEdge('E', 'F', 1);
+    graph.addEdge('E', 'G', 2);
+    graph.addEdge('G', 'A', 1);
+    graph.addEdge('G', 'B', 2);
+    // graph.addEdge('F', 'A', 2);
 
     expect(graph.vertexSize).toBe(7);
     expect(graph.edgeSize).toBe(11);
 });
 
 test('graph.removeEdge(vs, ve)', () => {
-    graph.removeEdge(v7, v1);
+    graph.removeEdge('G', 'A');
 
     expect(graph.vertexSize).toBe(7);
     expect(graph.edgeSize).toBe(10);
 });
 
 test("Fail when try to remove non-existent edge", () => {
-    expect(() => graph.removeEdge(v7, v1)).toThrow(Error);
+    expect(() => graph.removeEdge('G', 'A')).toThrow(Error);
 });
 
 test("graph.removeVertex(vertex), also remove edges that contains 'vertex'", () => {
-    graph.removeVertex(v7);
+    graph.removeVertex('G');
 
     expect(graph.vertexSize).toBe(6);
     expect(graph.edgeSize).toBe(8);
@@ -68,97 +59,97 @@ test("graph.removeVertex(vertex), also remove edges that contains 'vertex'", () 
 test("graph.getDistance(vs, ve) (don't have negative edge)", () => {
     expect(graph.hasNegativeWeight()).toBe(false);
 
-    expect(graph.getDistance(v1, v1)).toBe(0);
-    expect(graph.getDistance(v1, v2)).toBe(1);
-    expect(graph.getDistance(v3, v2)).toBe(3);
-    expect(graph.getDistance(v3, v4)).toBe(3);
-    expect(graph.getDistance(v3, v6)).toBe(3);
-    expect(graph.getDistance(v2, v5)).toBe(5);
-    expect(graph.getDistance(v5, v1)).toBe(Number.MAX_SAFE_INTEGER);
-    expect(graph.getDistance(v6, v1)).toBe(Number.MAX_SAFE_INTEGER);
+    expect(graph.getDistance('A', 'A')).toBe(0);
+    expect(graph.getDistance('A', 'B')).toBe(1);
+    expect(graph.getDistance('C', 'B')).toBe(3);
+    expect(graph.getDistance('C', 'D')).toBe(3);
+    expect(graph.getDistance('C', 'F')).toBe(3);
+    expect(graph.getDistance('B', 'E')).toBe(5);
+    expect(graph.getDistance('E', 'A')).toBe(Number.MAX_SAFE_INTEGER);
+    expect(graph.getDistance('F', 'A')).toBe(Number.MAX_SAFE_INTEGER);
 });
 
 test("graph.getPath(vs, ve) (don't have negative edge)", () => {
     expect(graph.hasNegativeWeight()).toBe(false);
 
-    expect(graph.getPath(v1, v1)).toEqual([v1]);
-    expect(graph.getPath(v3, v3)).toEqual([v3]);
-    expect(graph.getPath(v1, v2)).toEqual([v1, v2]);
-    expect(graph.getPath(v1, v4)).toEqual([v1, v3, v4]);
-    expect(graph.getPath(v1, v5)).toEqual([v1, v3, v5]);
-    expect(graph.getPath(v6, v1)).toEqual([]);
-    expect(graph.getPath(v5, v1)).toEqual([]);
+    expect(graph.getPath('A', 'A')).toEqual(['A']);
+    expect(graph.getPath('C', 'C')).toEqual(['C']);
+    expect(graph.getPath('A', 'B')).toEqual(['A', 'B']);
+    expect(graph.getPath('A', 'D')).toEqual(['A', 'C', 'D']);
+    expect(graph.getPath('A', 'E')).toEqual(['A', 'C', 'E']);
+    expect(graph.getPath('F', 'A')).toEqual([]);
+    expect(graph.getPath('E', 'A')).toEqual([]);
 });
 
 test("graph.getDistance(vs, ve), using bellman-ford algorithm.", () => {
-    expect(graph.bellmanFord(v1, v1)[0]).toBe(0);
-    expect(graph.bellmanFord(v1, v2)[0]).toBe(1);
-    expect(graph.bellmanFord(v3, v2)[0]).toBe(3);
-    expect(graph.bellmanFord(v3, v4)[0]).toBe(3);
-    expect(graph.bellmanFord(v3, v6)[0]).toBe(3);
-    expect(graph.bellmanFord(v2, v5)[0]).toBe(5);
-    expect(graph.bellmanFord(v5, v1)[0]).toBe(Number.MAX_SAFE_INTEGER);
-    expect(graph.bellmanFord(v6, v1)[0]).toBe(Number.MAX_SAFE_INTEGER);
+    expect(graph.bellmanFord('A', 'A')[0]).toBe(0);
+    expect(graph.bellmanFord('A', 'B')[0]).toBe(1);
+    expect(graph.bellmanFord('C', 'B')[0]).toBe(3);
+    expect(graph.bellmanFord('C', 'D')[0]).toBe(3);
+    expect(graph.bellmanFord('C', 'F')[0]).toBe(3);
+    expect(graph.bellmanFord('B', 'E')[0]).toBe(5);
+    expect(graph.bellmanFord('E', 'A')[0]).toBe(Number.MAX_SAFE_INTEGER);
+    expect(graph.bellmanFord('F', 'A')[0]).toBe(Number.MAX_SAFE_INTEGER);
 });
 
 test("graph.getPath(vs, ve), using bellman-ford algorithm.", () => {
-    expect(graph.bellmanFord(v1, v1)[1]).toEqual([v1]);
-    expect(graph.bellmanFord(v3, v3)[1]).toEqual([v3]);
-    expect(graph.bellmanFord(v1, v2)[1]).toEqual([v1, v2]);
-    expect(graph.bellmanFord(v1, v4)[1]).toEqual([v1, v3, v4]);
-    expect(graph.bellmanFord(v1, v5)[1]).toEqual([v1, v3, v5]);
-    expect(graph.bellmanFord(v6, v1)[1]).toEqual([]);
-    expect(graph.bellmanFord(v5, v1)[1]).toEqual([]);
+    expect(graph.bellmanFord('A', 'A')[1]).toEqual(['A']);
+    expect(graph.bellmanFord('C', 'C')[1]).toEqual(['C']);
+    expect(graph.bellmanFord('A', 'B')[1]).toEqual(['A', 'B']);
+    expect(graph.bellmanFord('A', 'D')[1]).toEqual(['A', 'C', 'D']);
+    expect(graph.bellmanFord('A', 'E')[1]).toEqual(['A', 'C', 'E']);
+    expect(graph.bellmanFord('F', 'A')[1]).toEqual([]);
+    expect(graph.bellmanFord('E', 'A')[1]).toEqual([]);
 });
 
 test("graph.updateEdgeWeight(vs, ve) to negative number.", () => {
-    graph.updateEdgeWeight(v1, v2, -2)
+    graph.updateEdgeWeight('A', 'B', -2)
 
-    expect(graph.getWeight(v1, v2)).toBe(-2);
+    expect(graph.getWeight('A', 'B')).toBe(-2);
 });
 
 test("graph.getDistance(vs, ve), using bellman-ford algorithm include negative weight.", () => {
     expect(graph.hasNegativeWeight()).toBe(true);
 
-    expect(graph.bellmanFord(v1, v1)[0]).toBe(0);
-    expect(graph.bellmanFord(v1, v2)[0]).toBe(-2);
-    expect(graph.bellmanFord(v3, v2)[0]).toBe(0);
-    expect(graph.bellmanFord(v3, v4)[0]).toBe(3);
-    expect(graph.bellmanFord(v3, v6)[0]).toBe(3);
-    expect(graph.bellmanFord(v2, v5)[0]).toBe(5);
-    expect(graph.bellmanFord(v5, v1)[0]).toBe(Number.MAX_SAFE_INTEGER);
-    expect(graph.bellmanFord(v6, v1)[0]).toBe(Number.MAX_SAFE_INTEGER);
+    expect(graph.bellmanFord('A', 'A')[0]).toBe(0);
+    expect(graph.bellmanFord('A', 'B')[0]).toBe(-2);
+    expect(graph.bellmanFord('C', 'B')[0]).toBe(0);
+    expect(graph.bellmanFord('C', 'D')[0]).toBe(3);
+    expect(graph.bellmanFord('C', 'F')[0]).toBe(3);
+    expect(graph.bellmanFord('B', 'E')[0]).toBe(5);
+    expect(graph.bellmanFord('E', 'A')[0]).toBe(Number.MAX_SAFE_INTEGER);
+    expect(graph.bellmanFord('F', 'A')[0]).toBe(Number.MAX_SAFE_INTEGER);
 });
 
 test("graph.getPath(vs, ve), using bellman-ford algorithm include negative weight.", () => {
     expect(graph.hasNegativeWeight()).toBe(true);
 
-    expect(graph.bellmanFord(v1, v1)[1]).toEqual([v1]);
-    expect(graph.bellmanFord(v3, v3)[1]).toEqual([v3]);
-    expect(graph.bellmanFord(v3, v2)[1]).toEqual([v3, v1, v2]);
-    expect(graph.bellmanFord(v1, v2)[1]).toEqual([v1, v2]);
-    expect(graph.bellmanFord(v1, v4)[1]).toEqual([v1, v2, v3, v4]);
-    expect(graph.bellmanFord(v1, v5)[1]).toEqual([v1, v2, v3, v5]);
-    expect(graph.bellmanFord(v6, v1)[1]).toEqual([]);
-    expect(graph.bellmanFord(v5, v1)[1]).toEqual([]);
+    expect(graph.bellmanFord('A', 'A')[1]).toEqual(['A']);
+    expect(graph.bellmanFord('C', 'C')[1]).toEqual(['C']);
+    expect(graph.bellmanFord('C', 'B')[1]).toEqual(['C', 'A', 'B']);
+    expect(graph.bellmanFord('A', 'B')[1]).toEqual(['A', 'B']);
+    expect(graph.bellmanFord('A', 'D')[1]).toEqual(['A', 'B', 'C', 'D']);
+    expect(graph.bellmanFord('A', 'E')[1]).toEqual(['A', 'B', 'C', 'E']);
+    expect(graph.bellmanFord('F', 'A')[1]).toEqual([]);
+    expect(graph.bellmanFord('E', 'A')[1]).toEqual([]);
 });
 
 test("graph.getDistance(vs, ve), using bellman-ford algorithm include negative weight.", () => {
-    graph.updateEdgeWeight(v1, v2, -6)
+    graph.updateEdgeWeight('A', 'B', -6)
 
     expect(graph.hasNegativeWeight()).toBe(true);
 
-    expect(() => graph.bellmanFord(v1, v1)).toThrow(Error);
-    expect(() => graph.bellmanFord(v1, v2)).toThrow(Error);
-    expect(() => graph.bellmanFord(v3, v2)).toThrow(Error);
-    expect(() => graph.bellmanFord(v3, v4)).toThrow(Error);
+    expect(() => graph.bellmanFord('A', 'A')).toThrow(Error);
+    expect(() => graph.bellmanFord('A', 'B')).toThrow(Error);
+    expect(() => graph.bellmanFord('C', 'B')).toThrow(Error);
+    expect(() => graph.bellmanFord('C', 'D')).toThrow(Error);
 });
 
 test("graph.getPath(vs, ve), using bellman-ford algorithm include 'negative cycle'.", () => {
     expect(graph.hasNegativeWeight()).toBe(true);
 
-    expect(() => graph.bellmanFord(v1, v1)[1]).toThrow(Error);
-    expect(() => graph.bellmanFord(v3, v3)[1]).toThrow(Error);
-    expect(() => graph.bellmanFord(v3, v2)[1]).toThrow(Error);
-    expect(() => graph.bellmanFord(v1, v2)[1]).toThrow(Error);
+    expect(() => graph.bellmanFord('A', 'A')[1]).toThrow(Error);
+    expect(() => graph.bellmanFord('C', 'C')[1]).toThrow(Error);
+    expect(() => graph.bellmanFord('C', 'B')[1]).toThrow(Error);
+    expect(() => graph.bellmanFord('A', 'B')[1]).toThrow(Error);
 });
--- test/UndirectedGraph.test.js
+++ test/UndirectedGraph.test.js
@@ -1,15 +1,6 @@
 let UndirectedGraph = require('../index').UndirectedGraph;
-let Vertex = UndirectedGraph.Vertex;
 
 let graph = new UndirectedGraph();
-
-let v1 = new Vertex('A');
-let v2 = new Vertex('B');
-let v3 = new Vertex('C');
-let v4 = new Vertex('D');
-let v5 = new Vertex('E');
-let v6 = new Vertex('F');
-let v7 = new Vertex('G');
 
 test('When construct new Graph,',()=>{
     expect(graph.vertexSize).toBe(0);
@@ -17,71 +8,75 @@ test('When construct new Graph,',()=>{
 });
 
 test('graph.addVertex(vertex)',()=>{
-    graph.addVertex(v1);
-    graph.addVertex(v2);
-    graph.addVertex(v3);
-    graph.addVertex(v4);
-    graph.addVertex(v5);
-    graph.addVertex(v6);
-    graph.addVertex(v7);
+    graph.addVertex('A');
+    graph.addVertex('B');
+    graph.addVertex('C');
+    graph.addVertex('D');
+    graph.addVertex('E');
+    graph.addVertex('F');
+    graph.addVertex('G');
+    graph.addVertex(1);
 
-    expect(graph.vertexSize).toBe(7);
+    expect(graph.vertexSize).toBe(8);
     expect(graph.edgeSize).toBe(0);
 });
 
 test('graph.addEdge(vs, ve, weight)',()=>{
-    graph.addEdge(v1, v2, 1);
-    graph.addEdge(v1, v3, 2);
-    graph.addEdge(v2, v3, 3);
-    graph.addEdge(v3, v4, 3);
-    graph.addEdge(v3, v5, 2);
-    graph.addEdge(v4, v6, 4);
-    graph.addEdge(v5, v6, 1);
-    graph.addEdge(v5, v7, 2);
-    graph.addEdge(v7, v1, 1);
-    graph.addEdge(v7, v2, 2);
-    // graph.addEdge(v6, v1, 2);
+    graph.addEdge('A', 'B', 1);
+    graph.addEdge('A', 'C', 2);
+    graph.addEdge('B', 'C', 3);
+    graph.addEdge('C', 'D', 3);
+    graph.addEdge('C', 'E', 2);
+    graph.addEdge('D', 'F', 4);
+    graph.addEdge('E', 'F', 1);
+    graph.addEdge('E', 'G', 2);
+    graph.addEdge('G', 'A', 1);
+    graph.addEdge('G', 'B', 2);
+    graph.addEdge('A', 1, 2);
+    // graph.addEdge('F', 'A', 2);
     
-    expect(graph.vertexSize).toBe(7);
-    expect(graph.edgeSize).toBe(10);
+    expect(graph.vertexSize).toBe(8);
+    expect(graph.edgeSize).toBe(11);
 });
 
 test('graph.removeEdge(vs, ve)',()=>{
-    graph.removeEdge(v7,v1);
+    graph.removeEdge('G','A');
 
-    expect(graph.vertexSize).toBe(7);
-    expect(graph.edgeSize).toBe(9);
+    expect(graph.vertexSize).toBe(8);
+    expect(graph.edgeSize).toBe(10);
 });
 
 test("Fail when try to remove non-existent edge", () => {
-    expect(()=>graph.removeEdge(v7,v1)).toThrow(Error);
+    expect(()=>graph.removeEdge('G','A')).toThrow(Error);
 });
 
 test("graph.removeVertex(vertex), also remove edges that contains 'vertex'",()=>{
-    graph.removeVertex(v7);
+    graph.removeVertex('G');
 
-    expect(graph.vertexSize).toBe(6);
-    expect(graph.edgeSize).toBe(7);
+    expect(graph.vertexSize).toBe(7);
+    expect(graph.edgeSize).toBe(8);
 });
 
 test("graph.getDistance(vs, ve)",()=>{
-    expect(graph.getDistance(v1,v1)).toBe(0);
-    expect(graph.getDistance(v4,v4)).toBe(0);
-    expect(graph.getDistance(v1,v2)).toBe(1);
-    expect(graph.getDistance(v3,v2)).toBe(3);
-    expect(graph.getDistance(v3,v4)).toBe(3);
-    expect(graph.getDistance(v3,v6)).toBe(3);
-    expect(graph.getDistance(v2,v5)).toBe(5);
-    expect(graph.getDistance(v5,v1)).toBe(4);
-    expect(graph.getDistance(v6,v1)).toBe(5);
+    expect(graph.getDistance('A','A')).toBe(0);
+    expect(graph.getDistance('D','D')).toBe(0);
+    expect(graph.getDistance('A','B')).toBe(1);
+    expect(graph.getDistance('C','B')).toBe(3);
+    expect(graph.getDistance('C','D')).toBe(3);
+    expect(graph.getDistance('C','F')).toBe(3);
+    expect(graph.getDistance('B','E')).toBe(5);
+    expect(graph.getDistance('E','A')).toBe(4);
+    expect(graph.getDistance('F','A')).toBe(5);
+    expect(graph.getDistance('A',1)).toBe(2);
 });
 
 test("graph.getPath(vs, ve)",()=>{
-    expect(graph.getPath(v1,v1)).toEqual([v1]);
-    expect(graph.getPath(v3,v3)).toEqual([v3]);
-    expect(graph.getPath(v1,v2)).toEqual([v1,v2]);
-    expect(graph.getPath(v1,v4)).toEqual([v1,v3,v4]);
-    expect(graph.getPath(v1,v5)).toEqual([v1,v3,v5]);
-    expect(graph.getPath(v6,v1)).toEqual([v6,v5,v3,v1]);
-    expect(graph.getPath(v5,v1)).toEqual([v5,v3,v1]);
+    expect(graph.getPath('A','A')).toEqual(['A']);
+    expect(graph.getPath('C','C')).toEqual(['C']);
+    expect(graph.getPath('A','B')).toEqual(['A','B']);
+    expect(graph.getPath('A','D')).toEqual(['A','C','D']);
+    expect(graph.getPath('A','E')).toEqual(['A','C','E']);
+    expect(graph.getPath('F','A')).toEqual(['F','E','C','A']);
+    expect(graph.getPath('E','A')).toEqual(['E','C','A']);
+    expect(graph.getPath('A',1)).toEqual(['A',1]);
 });
